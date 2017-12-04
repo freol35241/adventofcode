f25f857e4ab9336112c0445e46d431d5390e1ca2
freol35241 <freol@outlook.com>
2017-12-04 19:25:03 +0100

    Second part of day 4

--- day_04.py
+++ day_04.py
@@ -534,6 +534,28 @@ valid_phrases = [phrase for phrase in phrases if len(phrase)==len(np.unique(phra
 
 print(len(valid_phrases))
 
+"""
+
+--- Part Two ---
+
+For added security, yet another system policy has been put in place. Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+
+For example:
+
+    abcde fghij is a valid passphrase.
+    abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+    a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+    iiii oiii ooii oooi oooo is valid.
+    oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+
+Under this new system policy, how many passphrases are valid?
+"""
+
+
+valid_phrases = [phrase for phrase in phrases if len(phrase)==len(np.unique([''.join(sorted(word)) for word in phrase]))]
+
+print(len(valid_phrases))
+
 
 
 
